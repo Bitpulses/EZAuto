d7d353fbb29808c192bad8be00ef4d577a361547
Dot4diw <73592345+Dot4diw@users.noreply.github.com>
2026-06-03 19:34:43 +0800

    Update build script

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo Building EZAuto...
 set GCC_PATH=D:\Software\msys64\ucrt64\bin
 set PATH=%GCC_PATH%;%PATH%
 
-g++ -std=c++17 -O2 -o EZAuto.exe ^
+g++ -std=c++17 -O2 -o release/EZAuto.exe ^
     src/main.cpp ^
     src/FocusMonitor.cpp ^
     src/ImeSwitcher.cpp ^
