b31d4646334a482a4199b8084bb8a9463c34c3a5
Dot4diw <73592345+Dot4diw@users.noreply.github.com>
2026-06-04 09:03:47 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set GCC_PATH=D:\Software\msys64\ucrt64\bin
 #set GCC_PATH=D:\MySoftware\MSYS2\ucrt64\bin
 set PATH=%GCC_PATH%;%PATH%
 
-g++ -std=c++17 -O2 -static -o release/EZAuto.exe ^
+g++ -std=c++17 -O2 -o release/EZAuto.exe ^
     src/main.cpp ^
     src/FocusMonitor.cpp ^
     src/ImeSwitcher.cpp ^
